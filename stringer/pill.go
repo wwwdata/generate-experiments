a55995808be6dbdc7f55be3315167e6209ecd10b
Ben Bieker <mail@ben-bieker.de>
2016-01-26 22:53:15 +0100

    annotate with go:generate tag

--- stringer/pill.go
+++ stringer/pill.go
@@ -6,30 +6,18 @@ type Pill int
 
 // defining constants of the new type Pill
 // iota counts from 0 and increases in steps by 1 (in case you didn't know this feature)
+//go:generate stringer -type=Pill
 const (
 	Placebo Pill = iota
 	Aspirin
 	Ibuprofen
 	Paracetamol
+	NewPill
 	Acetaminophen = Paracetamol // Acetaminophen is the same as Paracetamol
 )
 
-// Implements the Stringer interface
-func (p Pill) String() string {
-	switch p {
-	case Placebo:
-		return "Placebo"
-	case Aspirin:
-		return "Aspirin"
-	case Ibuprofen:
-		return "Ibuprofen"
-	case Paracetamol: // == Acetaminophen
-		return "Paracetamol"
-	}
-	return fmt.Sprintf("Pill(%d)", p)
-}
-
 func main() {
 	var p Pill = Ibuprofen
-	fmt.Printf("Taking a %s pill\n", p)
+	n := NewPill
+	fmt.Printf("Taking a %s pill and a %s pill\n", p, n)
 }
