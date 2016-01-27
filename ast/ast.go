3c5e9f3c9e9979651a7eb8829863fbb09e09028f
Ben Bieker <mail@ben-bieker.de>
2016-01-27 22:03:58 +0100

    or comments

--- ast/ast.go
+++ ast/ast.go
@@ -15,4 +15,15 @@ func main() {
 	for _, i := range file.Imports {
 		fmt.Println(i.Path.Value)
 	}
+
+	// parsing comments
+	file, err = parser.ParseFile(token.NewFileSet(), "ast.go", nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	// such comment wow
+	for _, i := range file.Comments {
+		fmt.Println(i.Text())
+	}
 }
