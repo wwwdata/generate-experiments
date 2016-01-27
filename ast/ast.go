39f2bb89ffabb178168868a03331dbfd854059be
Ben Bieker <mail@ben-bieker.de>
2016-01-27 22:30:54 +0100

    next: how to write a const parser?

--- ast/ast.go
+++ ast/ast.go
@@ -1,42 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-)
-
+// how to parse consts of a type?
 func main() {
-	file, err := parser.ParseFile(token.NewFileSet(), "ast.go", nil, parser.ImportsOnly)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, i := range file.Imports {
-		fmt.Println(i.Path.Value)
-	}
-
-	// parsing comments
-	file, err = parser.ParseFile(token.NewFileSet(), "ast.go", nil, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
-
-	// such comment wow
-	for _, i := range file.Comments {
-		fmt.Println(i.Text())
-	}
-
-	// Parse very small example go program and inspect all the nodes
-	fset := token.NewFileSet()
+	// example go file that we want to parse all the Unicorn constants out of
 	src := `
-	package foo
-	const c = 1.0
+		package foo
+
+		type Unicorn int
+
+		const (
+			Pink Unicorn = iota
+			Fluffy
+			Rainbow
+		)
 	`
-	f, err := parser.ParseFile(fset, "something.go", src, 0)
-	ast.Inspect(f, func(n ast.Node) bool {
-		fmt.Printf("%#v\n", n)
-		return true
-	})
 }
