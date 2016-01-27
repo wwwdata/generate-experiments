a91cc2657668f875a7b75f3d08d94a9cedab2ec5
Ben Bieker <mail@ben-bieker.de>
2016-01-27 22:49:56 +0100

    output consts and types

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
 // how to parse consts of a type?
 func main() {
 	// example go file that we want to parse all the Unicorn constants out of
@@ -14,4 +21,37 @@ func main() {
 			Rainbow
 		)
 	`
+
+	file, err := parser.ParseFile(token.NewFileSet(), "unicorns.go", src, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	var values []string
+	// we now hard code the type for the sake of simplicity. The go:generator tool would read the type via a
+	// command line parameter like the Stringer oder Jsonenum tool
+	typeName := "Unicorn"
+
+	ast.Inspect(file, func(node ast.Node) bool {
+		decl, ok := node.(*ast.GenDecl)
+		if !ok || decl.Tok != token.CONST {
+			// we need consts
+			return true
+		}
+
+		for _, spec := range decl.Specs {
+			vspec := spec.(*ast.ValueSpec) // must work, because we are in a const
+
+			// let's see if we have a type for the const
+			fmt.Println("type: ", vspec.Type)
+
+			// outputs one line of code
+			fmt.Println(vspec.Names)
+		}
+
+		return false
+	})
+
+	fmt.Println("just to compile it")
+	fmt.Println(values, typeName)
 }
